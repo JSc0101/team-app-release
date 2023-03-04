f4866aaf6a2ae2297d2eb8caa5e3eb418994a214
Sebastian009w <ia3266505@gmail.com>
2023-03-04 18:15:24 -0500

    rust init --ini

---
+++ src/TypeScript/hello-world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\NICOLAS\Desktop\realease\src\TypeScript\hello-world\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Users\NICOLAS\Desktop\realease\src\TypeScript\hello-world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
